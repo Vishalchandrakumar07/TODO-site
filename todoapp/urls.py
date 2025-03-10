7046e8e4e8b4b973559e4435a3081ddf4aa784e0
vishalchandrakumar07 <vishalchandrakumar07@gmail.com>
2025-03-10 21:42:49 +0530

    first commit

---
+++ todoapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('', views.home, name = 'home-page'),
+    path('logout/', views.LogoutView, name='logout'),
+    path('register/', views.register, name='register'),
+    path('login/', views.loginpage, name='login'),
+    path('delete/<int:id>/', views.delete_task, name='delete'),
+    path('update/<int:id>/', views.update_task, name='update'), 
+]
