b0535e09df71c88f6603fdf8a2380cb17088ea2f
vishalchandrakumar07 <vishalchandrakumar07@gmail.com>
2025-03-20 14:35:28 +0530

    urls

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.home, name = 'home-page'),
-    path('logout/', views.LogoutView, name='logout'),
-    path('register/', views.register, name='register'),
-    path('login/', views.loginpage, name='login'),
+    # path('logout/', views.LogoutView, name='logout'),
+    # path('register/', views.register, name='register'),
+    # path('login/', views.loginpage, name='login'),
     path('delete/<int:id>/', views.delete_task, name='delete'),
     path('update/<int:id>/', views.update_task, name='update'), 
 ]
